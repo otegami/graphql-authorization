08ec73f354a5e63166d669263f98d221a9a69c06
otegami <a.s.takuya1026@gmail.com>
2022-08-20 16:31:30 +0900

    Project にアサインされているスタッフを取得できるように AssignedMemberType を定義
    Member の address に関しては、機密情報なので、Project 経由ではアクセスできない

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
 projects = [
-  { name: '猫プロジェクト', description: 'プロジェクトマネージャーにアサインされたプロジェクト' },
-  { name: '犬プロジェクト', description: 'チーフプロジェクトマネージャーのみ見れる極秘プロジェクト' }
+  { id: 1, name: '猫プロジェクト', description: 'プロジェクトマネージャーにアサインされたプロジェクト' },
+  { id: 2, name: '犬プロジェクト', description: 'チーフプロジェクトマネージャーのみ見れる極秘プロジェクト' }
 ]
 members = [
-  { name: 'スコティッシュフォールド', address: '猫小屋一丁目一番地' },
-  { name: 'アメリカンショートヘア', address: '猫小屋二丁目二番地' },
-  { name: '柴犬', address: '犬小屋一丁目' }
+  { id: 1, name: 'スコティッシュフォールド', address: '猫小屋一丁目一番地' },
+  { id: 2, name: 'アメリカンショートヘア', address: '猫小屋二丁目二番地' },
+  { id: 3, name: '柴犬', address: '犬小屋一丁目' }
+]
+assignings = [
+  { id: 1, project_id: 1, member_id: 1 },
+  { id: 2, project_id: 1, member_id: 2 },
+  { id: 3, project_id: 2, member_id: 3 }
 ]
 
 ActiveRecord::Base.transaction do
   Project.insert_all!(projects)
   Member.insert_all!(members)
+  Assigning.insert_all!(assignings)
 end
