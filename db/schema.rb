9ddfed1d1774131c77bcc11fc670c37edf73f8ae
otegami <a.s.takuya1026@gmail.com>
2022-08-20 10:09:57 +0900

    rails db:migrate about projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_010114) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_010732) do
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.integer "role", null: false
