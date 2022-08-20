47806c1b0fcbdbdad374b8b63179474545cd3f2d
otegami <a.s.takuya1026@gmail.com>
2022-08-20 10:13:49 +0900

    rails db:migrate about members

--- db/migrate/20220820011140_create_members.rb
+++ db/migrate/20220820011140_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[7.0]
   def change
     create_table :members do |t|
-      t.string :name
-      t.string :address
+      t.string :name, null: false
+      t.string :address, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_010732) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_011140) do
+  create_table "members", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
