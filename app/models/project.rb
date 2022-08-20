a2a58693166c821206b4af70e6b1ccb51d4b01de
otegami <a.s.takuya1026@gmail.com>
2022-08-20 10:23:46 +0900

    Porject と User のアソシエーションを設定

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_many :manaings
+  has_many :users, thorugh: :manaings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :manaings
+  has_many :projects, thorugh: :manaings
 end
